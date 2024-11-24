374db8ab990195859f3cf29e4b9771659bd1a9f8
Aziz0307 <Aziz03072018@gmail.com>
2024-11-24 12:59:15 +0500

    Boolean16

---
+++ Boolean16/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Boolean16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vvedite chetnoe dvuznachnoe chislo: ");
+            var a = int.Parse(Console.ReadLine());
+            bool result =(a>=10 && a<=99 && a % 2 ==0) ? true: false;
+            Console.WriteLine(result);
+        }
+    }
+}
